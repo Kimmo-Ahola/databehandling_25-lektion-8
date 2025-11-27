65bac7c06a788d7fb026649bf6f0938967d6808c
Kimmo <kimmo.ahola@systementor.se>
2025-11-27 08:27:02 +0100

    pull over from last lesson

--- main.py
+++ main.py
@@ -8,71 +8,29 @@ from models.category import Category
 from models.customer import Customer
 
 
-"""
-different query examples
-This is the absolute basics on how to do CRUD:
-
-use My_Session as session:
-    session.add(object) # add to db
-    session.delete(object) # delete from db
-
-    session.commit() # save changes
-"""
 with My_Session() as session:
-    pass
+    category_count = session.query(Category).count()
 
-    ## We add a category the database. categoryname is unique, so we need to check for errors.
-    ## try: except: or do a query to check if name exists
+    if category_count == 0:
+        category = Category(categoryname="Beverage", 
+                            description="Drinks, soft drinks, hard spirits", 
+                            picture="www.google.com")
+        
+        session.add(category)
+        session.commit()
 
-    # category = Category(categoryname="Beverage", 
-    #                     description="Drinks, soft drinks, hard spirits", 
-    #                     picture="www.google.com")
+    customer_count = session.query(Customer).count()
+
+    if customer_count == 0:
+        customer = Customer(
+            first_name="Kimmo", 
+            last_name="Ahola",
+            email="kimmo@mail.se",
+            address="Storgatan 1",
+            city="Uppsala",
+            postal_code="757 55")
+        
+        session.add(customer)
+        session.commit()
+
     
-    # session.add(category)
-
-    ## get all_users as List[Customer] withh .all()
-    # all_users = session.query(Customer).all()
-    ## loop through and delete users
-    # for user in all_users:
-    #     session.delete(user)
-    # session.commit()
-
-    ## SELECT COUNT(*) FROM CUSTOMERS -- We get the number of rows in our table
-    # count = session.query(Customer).count()
-
-    ## if table is empty: create customers
-    # if count == 0:
-    #     users = Customer.create_customers()
-
-    #     # for user in users:
-    #     #     session.add(user)
-
-    #     session.add_all(users)
-
-    #     session.commit()
-
-    ## find all customers with first_name "Kalle", order by last_name descending
-    # query = session.query(Customer)\
-    #     .where(Customer.first_name == "Kalle")\
-    #     .order_by(Customer.last_name.desc())\
-    #     .all()
-
-
-    ## Find the first row in the category table
-    # query = session.query(Category)
-    # result = query.first()
-
-    ## .first() returns Category | None so we have to check for that
-    # if result is not None:
-    #     print(result.id)
-    # else:
-    #     print("No categories exist")
-    # try:
-    #     query = session.query(Category).one()
-    #     print(query.id)
-    # except:
-    #     print("error here")
-
-    # session.commit()
-    
-
